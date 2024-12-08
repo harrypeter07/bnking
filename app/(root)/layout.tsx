075eb479071987ce18ce389fc140e48daf6bcf07
harrypeter07 <hassanmansuri570@gmail.com>
2024-12-08 22:15:41 +0530

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -5,10 +8,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName : 'Hassan' , lastName: 'Mansuri'};
   return (
-   <main>
-    SIDEBAR
+   <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn}/>
+
+    {/* sidebar for mobile*/}
+       <div className="flex size-full flex-col">
+           <div className="root-layout">
+             
+             <Image
+             src="/icons/logo.svg"
+             width={30}
+             height={30}
+             alt="menu icon"
+             />
+
+              <div>
+                <MobileNav user={loggedIn}/>
+              </div>
+           </div>
     {children}
+       </div>
    </main>
   );
 }
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>
+      Thisoty
+    </div>
+  )
+}
+
+export default TransactionHistory
