349b1d87e8fe142282e088c9f5a534cb5ec89b6f
harrypeter07 <hassanmansuri570@gmail.com>
2024-12-09 17:22:52 +0530

    Implement auth appwrite and sentry

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>
-      sign In
-    </div>
+    <section className='flex-center size-full
+    max-sm:px-6'>
+      <AuthForm type="sign-in"/>
+    </section>
   )
 }
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -66,9 +67,9 @@ export const formatDateTime = (dateString: Date) => {
 };
 
 export function formatAmount(amount: number): string {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
     minimumFractionDigits: 2,
   });
 
@@ -193,3 +194,21 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+
+export const authFormSchema =(type : string)=>  z.object({
+  email:z.string().email(),
+  password: z.string().min( 8 , 'Password must contain atleast 8 characters'),
+
+  //sign-up-only
+   
+  firstName: type === 'sign-in' ? z.string().optional()  : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional()  :z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional()  :z.string().max(50).min(6),
+  city: type === 'sign-in' ? z.string().optional()  :z.string().max(50).min(6),
+  state: type === 'sign-in' ? z.string().optional()  :z.string().min(3).max(16),
+  postalCode: type === 'sign-in' ? z.string().optional()  :z.string().min(6).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional()  :z.string().min(8).max(8),
+  aadhaarNumber: type === 'sign-in' ? z.string().optional()  :z.string().min(12).max(12),
+
+})
